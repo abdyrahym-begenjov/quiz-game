0b705cab089ff16bfbb0a62da0ef24f4b5e6391a
Abdyrahym Begenjov <abdurahymbegencev@gmail.com>
2026-05-27 23:29:18 +0500

    Version 1.2
    Added a "Danger" option to the "Timer" mode with a bomb trap gameplay element for random incorrect answer options. Added a rating system. Fixed some bugs.

--- Quiz.py
+++ Quiz.py
@@ -7,9 +7,9 @@ print('Created by Abdyrahym Begenjov     (GitHub: abdyrahym-begenjov)')
 
 while True:
     print('Timer      Infinity')
-    mode=input('Choose mode of game: ')
-    mode=mode.title().strip()
-    if mode=='Timer' or mode=='Infinity':
+    mode_game=input('Choose mode of game: ')
+    mode_game=mode_game.title().strip()
+    if mode_game=='Timer' or mode_game=='Infinity':
         break
 
 print('Loading...')
@@ -19,35 +19,39 @@ lst=pyread('q1.json')
 v=['A)', 'B)', 'C)', 'D)']
 
 points=0
-num=0
+regular=0
+irregular=0
+number=0
 heart=3
 
-if mode=='Timer':
+if mode_game=='Timer':
     while True:
-        print('Parameters of game: Easy (15), Normal (25), Hard (40)')
+        print('Parameters of game: Easy (15), Normal (25), Hard (40), Danger (40 with surprise)')
         parameter=input('Enter the parameter of game: ')
         parameter=parameter.title().strip()
         match parameter:
             case 'Easy':
-                num1=15
+                nums_question=15
+                seconds=45
                 break
             case 'Normal':
-                num1=25
+                nums_question=25
+                seconds=60
                 break
-            case 'Hard':
-                num1=40
+            case 'Hard' | 'Danger':
+                nums_question=40
+                seconds=90
                 break
             case _:
                 print('Error!!!')
-    seconds=60
-    print('Let\' go')
+    print('Let\'s go')
 else:
     print(f'You have {heart} ❤️')
 
-def func(timer=None, seconds=None):
+def answer_question(countdown=None, seconds=None):
     while True:
-        if timer and seconds:
-            answer=input(f'Time: {timer}. Enter the variant of answer: ')
+        if countdown and seconds:
+            answer=input(f'Time: {countdown}. Enter the variant of answer: ')
         else:
             answer=input('Enter the variant of answer: ')
         answer=answer.upper().strip()
@@ -55,54 +59,96 @@ def func(timer=None, seconds=None):
             break
         else:
             print('You must enter the variant!!!')
-    if timer:
+    if countdown:
         return answer, seconds
     else:
         return answer
 
 while True:
-    match mode:
+    match mode_game:
         case 'Timer':
             mins, secs=divmod(seconds, 60)
-            timer=f'{mins:02d}:{secs:02d}'
-            if num1==0:
-                print(f'Time: {timer}. You received {points} points.')
+            countdown=f'{mins:02d}:{secs:02d}'
+            if nums_question==0:
+                print(f'Time: {countdown}. Regular: {regular}  Irregular: {irregular}')
+                if regular==number:
+                    print('You Win!!!')
+                    print('⭐'*3)
+                    final=True
+                elif (regular/number)*100>=70:
+                    print('Good Work!!!')
+                    print('⭐'*2)
+                    final=True
+                elif (regular/number)*100>50:
+                    print('Weak, but still you\'re great!!!')
+                    print('⭐')
+                    final=True
+                elif regular==irregular:
+                    print('The final question to determine the outcome of the game.')
+                    nums_question+=1
+                    continue
+                else:
+                    print('Game Over!!!')
+                    print('💩')
+                    final=True
+                seconds=mins*60+secs
+                if final:
+                    points=regular+(seconds/(irregular+1))
+                print(f'Points: {int(points)}')
                 break
             elif seconds<=0:
                 print(f'Time: 00:00. Game Over!!!')
                 break
         case 'Infinity':
             if lst==[]:
-                print('You an ABSOLUTE CHAMPION!!!')
+                print('You are ABSOLUTE CHAMPION!!!')
                 break
             elif heart==0:
                 print('Game Over!!!')
                 print(f'Our record: {points} points.')
                 break
-    num+=1
+    number+=1
     question=choice(lst)
-    print(f'{num}) {question[0]}')
+    print(f'{number}) {question[0]}')
     print(f'{v[0]} {question[1]:<35} {" ":>35} {v[2]} {question[3]}')
     print(f'{v[1]} {question[2]:<35} {" ":>35} {v[3]} {question[4]}')
-    if mode=='Timer':
+    if mode_game=='Timer':
         start=time()
-        answer, seconds=func(timer, seconds)
+        answer, seconds=answer_question(countdown, seconds)
         end=time()
         seconds=seconds-int(end-start)
     else:
-        answer=func()
+        answer=answer_question()
     if answer==question[5]:
         print('Yes')
-        points+=1
-        if mode=='Timer':
+        if mode_game=='Timer':
+            regular+=1
             seconds+=5
+        else:
+            points+=1
     else:
         print('No')
-        if mode=='Infinity':
+        if mode_game=='Timer' and parameter=='Danger':
+            dict_variants={'Q': 0, 'A': 1, 'B': 2, 'C': 3, 'D': 4}
+            old_question=question.copy()
+            q=old_question[5]
+            old_question.pop(5)
+            old_question.pop(dict_variants[q])
+            dict_variants={j: i for i, j in dict_variants.items()}
+            old_question_index=[old_question.index(i) for i in old_question]
+            bomb_list=[dict_variants[i] for i in old_question_index]
+            bomb_list.remove('Q')
+            bomb=choice(bomb_list)
+            if answer==bomb:
+                print('💣 BOOM!!!')
+                seconds-=10
+        if mode_game=='Infinity':
             heart-=1
             print(f'You have {heart} ❤️.')
-    if mode=='Timer':
-        num1-=1
+        else:
+            irregular+=1
+    if mode_game=='Timer':
+        nums_question-=1
     lst.remove(question)
 
 end=input('Enter to exit: ')
