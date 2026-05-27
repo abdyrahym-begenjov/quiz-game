22121d289bfa9280ee4edab814a5e03e06ab3893
Abdyrahym Begenjov <abdurahymbegencev@gmail.com>
2026-05-27 14:37:55 +0500

    Version 1.1
    Replaced Simple Mode with Timer Mode (answer a certain number of questions depending on the parameter in one minute); improved the code

--- Quiz.py
+++ Quiz.py
@@ -1,15 +1,15 @@
 from random import choice
-from time import sleep
+from time import sleep, time
 from propython import pyread
 
 print('Quiz')
 print('Created by Abdyrahym Begenjov     (GitHub: abdyrahym-begenjov)')
 
 while True:
-    print('Simple  |  Infinity')
+    print('Timer      Infinity')
     mode=input('Choose mode of game: ')
     mode=mode.title().strip()
-    if mode=='Simple' or mode=='Infinity':
+    if mode=='Timer' or mode=='Infinity':
         break
 
 print('Loading...')
@@ -22,51 +22,86 @@ points=0
 num=0
 heart=3
 
-if mode=='Simple':
+if mode=='Timer':
     while True:
-        num1=(input('Enter the number of questions for game: '))
-        try:
-            num1=int(num1)
-            if num1>len(lst):
-                print('This number is bigger than number of list of question!!!')
-            else:
+        print('Parameters of game: Easy (15), Normal (25), Hard (40)')
+        parameter=input('Enter the parameter of game: ')
+        parameter=parameter.title().strip()
+        match parameter:
+            case 'Easy':
+                num1=15
                 break
-        except ValueError:
-            print('Error!!!')
+            case 'Normal':
+                num1=25
+                break
+            case 'Hard':
+                num1=40
+                break
+            case _:
+                print('Error!!!')
+    seconds=60
     print('Let\' go')
 else:
     print(f'You have {heart} ❤️')
 
-while True:
-    if mode=='Simple' and num1==0:
-        print(f'You received {points} points.')
-        break
-    elif mode=='Infinity' and heart==0:
-        print('Game Over!!!')
-        print(f'Our record: {points} points.')
-        break
-    else:
-        num+=1
-        question=choice(lst)
-        print(f'{num}) {question[0]}')
-        print(f'{v[0]} {question[1]:<35} {" ":>35} {v[2]} {question[3]}')
-        print(f'{v[1]} {question[2]:<35} {" ":>35} {v[3]} {question[4]}')
-        answer=input('Enter the variant of answer: ')
-        answer=answer.upper().strip()
-        if answer==question[5]:
-            print('Yes')
-            points+=1
-        elif answer=='':
-            print('You must enter the variant!!!')
-            if mode=='Infinity':
-                heart-=1
-                print(f'You have {heart} ❤️.')
+def func(timer=None, seconds=None):
+    while True:
+        if timer and seconds:
+            answer=input(f'Time: {timer}. Enter the variant of answer: ')
         else:
-            print('No')
-            if mode=='Infinity':
-                heart-=1
-                print(f'You have {heart} ❤️.')
-    if mode=='Simple':
+            answer=input('Enter the variant of answer: ')
+        answer=answer.upper().strip()
+        if answer in ['A', 'B', 'C', 'D']:
+            break
+        else:
+            print('You must enter the variant!!!')
+    if timer:
+        return answer, seconds
+    else:
+        return answer
+
+while True:
+    match mode:
+        case 'Timer':
+            mins, secs=divmod(seconds, 60)
+            timer=f'{mins:02d}:{secs:02d}'
+            if num1==0:
+                print(f'Time: {timer}. You received {points} points.')
+                break
+            elif seconds<=0:
+                print(f'Time: 00:00. Game Over!!!')
+                break
+        case 'Infinity':
+            if lst==[]:
+                print('You an ABSOLUTE CHAMPION!!!')
+                break
+            elif heart==0:
+                print('Game Over!!!')
+                print(f'Our record: {points} points.')
+                break
+    num+=1
+    question=choice(lst)
+    print(f'{num}) {question[0]}')
+    print(f'{v[0]} {question[1]:<35} {" ":>35} {v[2]} {question[3]}')
+    print(f'{v[1]} {question[2]:<35} {" ":>35} {v[3]} {question[4]}')
+    if mode=='Timer':
+        start=time()
+        answer, seconds=func(timer, seconds)
+        end=time()
+        seconds=seconds-int(end-start)
+    else:
+        answer=func()
+    if answer==question[5]:
+        print('Yes')
+        points+=1
+        if mode=='Timer':
+            seconds+=5
+    else:
+        print('No')
+        if mode=='Infinity':
+            heart-=1
+            print(f'You have {heart} ❤️.')
+    if mode=='Timer':
         num1-=1
     lst.remove(question)
 
