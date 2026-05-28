5265f287400bb9d0b4983bbe35e932f9e2b4c890
Abdyrahym Begenjov <abdurahymbegencev@gmail.com>
2026-05-28 21:25:20 +0500

    Version 1.3
    fix: bug fixes and add Russian language support
    - Added language selection (EN/RU).
    - Integrated custom `translator` module for UI strings.

--- Quiz.py
+++ Quiz.py
@@ -1,22 +1,39 @@
 from random import choice
 from time import sleep, time
 from propython import pyread
-
-print('Quiz')
-print('Created by Abdyrahym Begenjov     (GitHub: abdyrahym-begenjov)')
+from translator import *
 
 while True:
-    print('Timer      Infinity')
-    mode_game=input('Choose mode of game: ')
+    print('English  |  Русский')
+    chosen_language=input()
+    match chosen_language:
+        case 'English':
+            lang='en'
+            lst=pyread('q1.json')
+            v=['A', 'B', 'C', 'D']
+            break
+        case 'Русский':
+            lang='ru'
+            lst=pyread('q2.json')
+            v=['А', 'Б', 'В', 'Г']
+            break
+        case _:
+            print('Error!!!')
+
+print(translator('Quiz', lang))
+print(f'{translator('Creator: Abdyrahym Begenjov', lang)}     (GitHub: abdyrahym-begenjov)')
+
+while True:
+    print(translator('Timer      Infinity', lang))
+    mode_game=input(translator('Choose a game mode: ', lang))
     mode_game=mode_game.title().strip()
+    if lang=='ru':
+        mode_game=translator(mode_game, 'en1')
     if mode_game=='Timer' or mode_game=='Infinity':
         break
 
-print('Loading...')
+print(translator('Loading...', lang))
 sleep(2)
-
-lst=pyread('q1.json')
-v=['A)', 'B)', 'C)', 'D)']
 
 points=0
 regular=0
@@ -26,9 +43,11 @@ heart=3
 
 if mode_game=='Timer':
     while True:
-        print('Parameters of game: Easy (15), Normal (25), Hard (40), Danger (40 with surprise)')
-        parameter=input('Enter the parameter of game: ')
+        print(translator('Parameters of game: Easy (15), Normal (25), Hard (40), Dangerous (40 with surprise)', lang))
+        parameter=input(translator('Enter the parameter of game: ', lang))
         parameter=parameter.title().strip()
+        if lang=='ru':
+            parameter=translator(parameter, 'en1')
         match parameter:
             case 'Easy':
                 nums_question=15
@@ -38,27 +57,28 @@ if mode_game=='Timer':
                 nums_question=25
                 seconds=60
                 break
-            case 'Hard' | 'Danger':
+            case 'Hard' | 'Dangerous':
                 nums_question=40
                 seconds=90
                 break
             case _:
-                print('Error!!!')
-    print('Let\'s go')
+                print(translator('Error!!!', lang))
+    start_game=input(translator('Enter to start game: ', lang))
+    print(translator('Let\'s Go!!!', lang))
 else:
-    print(f'You have {heart} ❤️')
+    print(f'{translator('You have', lang)} {heart} ❤️')
 
 def answer_question(countdown=None, seconds=None):
     while True:
         if countdown and seconds:
-            answer=input(f'Time: {countdown}. Enter the variant of answer: ')
+            answer=input(f'{translator('Time: ', lang)}{countdown}. {translator('Enter the variant of answer: ', lang)}')
         else:
-            answer=input('Enter the variant of answer: ')
+            answer=input(translator('Enter the variant of answer: ', lang))
         answer=answer.upper().strip()
-        if answer in ['A', 'B', 'C', 'D']:
+        if answer in v:
             break
         else:
-            print('You must enter the variant!!!')
+            print(translator('You must enter the variant!!!', lang))
     if countdown:
         return answer, seconds
     else:
@@ -70,48 +90,48 @@ while True:
             mins, secs=divmod(seconds, 60)
             countdown=f'{mins:02d}:{secs:02d}'
             if nums_question==0:
-                print(f'Time: {countdown}. Regular: {regular}  Irregular: {irregular}')
+                print(f'{translator('Time: ', lang)}{countdown}. {translator('Regular: ', lang)}{regular}  {translator('Irregular: ', lang)}{irregular}')
                 if regular==number:
-                    print('You Win!!!')
+                    print(translator('You Win!!!', lang))
                     print('⭐'*3)
                     final=True
                 elif (regular/number)*100>=70:
-                    print('Good Work!!!')
+                    print(translator('Good Work!!!', lang))
                     print('⭐'*2)
                     final=True
                 elif (regular/number)*100>50:
-                    print('Weak, but still you\'re great!!!')
+                    print(translator('Weak, but still you\'re great!!!', lang))
                     print('⭐')
                     final=True
                 elif regular==irregular:
-                    print('The final question to determine the outcome of the game.')
+                    print(translator('The final question to determine the outcome of the game.', lang))
                     nums_question+=1
                     continue
                 else:
-                    print('Game Over!!!')
+                    print(translator('Game Over!!!', lang))
                     print('💩')
                     final=True
                 seconds=mins*60+secs
                 if final:
                     points=regular+(seconds/(irregular+1))
-                print(f'Points: {int(points)}')
+                print(f'{translator('Points: ', lang)}{int(points)}')
                 break
             elif seconds<=0:
-                print(f'Time: 00:00. Game Over!!!')
+                print(translator('Time: 00:00. Game Over!!!', lang))
                 break
         case 'Infinity':
             if lst==[]:
-                print('You are ABSOLUTE CHAMPION!!!')
+                print(translator('You are ABSOLUTE CHAMPION!!!', lang))
                 break
             elif heart==0:
-                print('Game Over!!!')
-                print(f'Our record: {points} points.')
+                print(translator('Game Over!!!', lang))
+                print(f'{translator('Our record: ', lang)}{points} {translator('points.', lang)}')
                 break
     number+=1
     question=choice(lst)
     print(f'{number}) {question[0]}')
-    print(f'{v[0]} {question[1]:<35} {" ":>35} {v[2]} {question[3]}')
-    print(f'{v[1]} {question[2]:<35} {" ":>35} {v[3]} {question[4]}')
+    print(f'{v[0]}) {question[1]:<35} {" ":>35} {v[2]}) {question[3]}')
+    print(f'{v[1]}) {question[2]:<35} {" ":>35} {v[3]}) {question[4]}')
     if mode_game=='Timer':
         start=time()
         answer, seconds=answer_question(countdown, seconds)
@@ -120,16 +140,16 @@ while True:
     else:
         answer=answer_question()
     if answer==question[5]:
-        print('Yes')
+        print(translator('Yes', lang))
         if mode_game=='Timer':
             regular+=1
             seconds+=5
         else:
             points+=1
     else:
-        print('No')
-        if mode_game=='Timer' and parameter=='Danger':
-            dict_variants={'Q': 0, 'A': 1, 'B': 2, 'C': 3, 'D': 4}
+        print(translator('No', lang))
+        if mode_game=='Timer' and parameter=='Dangerous':
+            dict_variants={'Q': 0, v[0]: 1, v[1]: 2, v[2]: 3, v[3]: 4}
             old_question=question.copy()
             q=old_question[5]
             old_question.pop(5)
@@ -140,15 +160,15 @@ while True:
             bomb_list.remove('Q')
             bomb=choice(bomb_list)
             if answer==bomb:
-                print('💣 BOOM!!!')
+                print(translator('💣 BOOM!!!', lang))
                 seconds-=10
         if mode_game=='Infinity':
             heart-=1
-            print(f'You have {heart} ❤️.')
+            print(f'{translator('You have', lang)} {heart} ❤️.')
         else:
             irregular+=1
     if mode_game=='Timer':
         nums_question-=1
     lst.remove(question)
 
-end=input('Enter to exit: ')
+end=input(translator('Enter to exit: ', lang))
---
+++ translator.py
@@ -0,0 +1,55 @@
+rutranslate={
+    'Quiz': 'Викторина',
+    'Timer      Infinity': 'Таймер      Бесконечность',
+    'Timer': 'Таймер',
+    'Infinity': 'Бесконечность',
+    'Parameters of game: Easy (15), Normal (25), Hard (40), Dangerous (40 with surprise)': 'Параметры игры: Лёгкий (15), Нормальный (25), Сложный (40), Опасный (40 с сюрпризом)',
+    'You have': 'У вас есть',
+    'Time: ': 'Время: ',
+    'Enter the variant of answer: ': 'Введите вариант ответа: ',
+    'You must enter the variant!!!': 'Вы должны ввести вариант!!!',
+    'Regular: ': 'Правильные: ',
+    'Irregular: ': 'Неправильные: ',
+    'You Win!!!': 'Вы выиграли!!!',
+    'Good Work!!!': 'Хорошая работа!!!',
+    'Weak, but still you\'re great!!!': 'Слабовато, но все же ты молодец!!!',
+    'The final question to determine the outcome of the game.': 'Последний вопрос, который определит исход игры.', 
+    'Game Over!!!': 'Игра окончена!!!',
+    'Points: ': 'Очки: ',
+    'Time: 00:00. Game Over!!!': 'Время: 00:00. Игра окончена!!!',
+    'You are ABSOLUTE CHAMPION!!!': 'Ты АБСОЛЮТНЫЙ ЧЕМПИОН!!!',
+    'Our record: ': 'Ваш рекорд: ',
+    'points.': 'очки. ',
+    'Yes': 'Да',
+    '💣 BOOM!!!': '💣 БУМ!!!',
+    'Error!!!': 'Ошибка!!!',
+    'Creator: Abdyrahym Begenjov': 'Создатель: Абдырахым Бегенджов',
+    'Enter to start game: ': 'Нажмите, чтобы начать игру: ',
+    'Loading...': 'Загрузка...',
+    'Enter the parameter of game: ': 'Введите параметр игры: ',
+    'Easy': 'Лёгкий',
+    'Normal': 'Нормальный',
+    'Hard': 'Сложный',
+    'Dangerous': 'Опасный',
+    'Enter to exit: ': 'Введите для выхода: ',
+    'Let\'s Go!!!': 'Начнём!!!',
+    'Choose a game mode: ': 'Выберите режим игры: ',
+    'No': 'Нет'
+             }
+
+
+entranslate={j: i for i, j in rutranslate.items()}
+
+
+def translator(word, language):
+    match language:
+        case 'en':
+            return word
+        case 'en1':
+            if word not in entranslate:
+                return 'Error!!!'
+            return entranslate[word]
+        case 'ru':
+            return rutranslate[word]
+        case _:
+            return '???'
